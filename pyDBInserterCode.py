8ca73ee4a7082610f35398a13302d46a4c9019a5
Sooraj Antony <soorajantony@live.in>
2017-08-10 17:32:45 +0530

    MySQL working interface configured

--- pyDBInserterCode.py
+++ pyDBInserterCode.py
@@ -1,48 +1,17 @@
-import cgi, cgitb 
-from datetime import datetime
-import pymysql
+#!/usr/bin/python
+import cgi
+import MySQLdb
 
-
-# DATABASE UPDATE
-connection = pymysql.connect(
-            host='localhost', db='admin_logger',
-            user='root', passwd='careersnow@123')
-mycursor = connection.cursor()
-
-
-
-##MYSQL Sscript to WRITE to mysql db
-##     sql="""SCRIPT TO PERFORM WRITES"""
-##     mycursor.execute(sql)    
-##     connection.commit() 
-
-
-
-
-
-    print "Content-type:text/html\r\n\r\n"
-    print "<html>"
-    print "<head>"
-    print "<title>Hello - Second CGI Program</title>"
-    print "</head>"
-    print "<body>"
-    sql="""SHOW TABLES;"""
-    for result in cursor.execute(sql, multi=True):
-      if result.with_rows:
-        print("<h6>Rows produced by statement '{}':</h6>".format(
-          result.statement))
-        print("<h6>"result.fetchall()"</h6>")
-      else:
-        print("<h6>Number of rows affected by statement '{}': {}</h6>".format(
-          result.statement, result.rowcount))
-    print "</body>"
-    print "</html>"
-
-
-
-
-
-
-
-mycursor.close()  ## here all loops done
-connection.close()  ## close db connection
+try:
+    db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="umn1234", db="admin_logger")
+    cursor = db.cursor()        
+    cursor.execute("SHOW TABLES;")
+    results = cursor.fetchall()
+    # Check if anything at all is returned
+    if results:
+        print results[0]
+    else:
+        print "getr"               
+except MySQLdb.Error:
+    print "ERROR IN CONNECTION"
+print "getrqw"
