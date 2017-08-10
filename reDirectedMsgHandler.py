3e4cae12efef3faee56b68c07fe0afed7a08f4d3
Sooraj Antony <soorajantony@live.in>
2017-08-10 19:11:33 +0530

    MsgHandler Service standalone code developed

--- reDirectedMsgHandler.py
+++ reDirectedMsgHandler.py
@@ -2,28 +2,42 @@
 
 # Import modules for CGI handling 
 import cgi, cgitb 
+from hashingFunction import hasher
 from datetime import datetime
 
 
-# Create instance of FieldStorage 
-form = cgi.FieldStorage()
-
-# Get data from fields
-
-def responseHandler():
-	tokenID = form.getvalue('token')
-	formID = form.getvalue('formIDs')
+#input values from response form
+def valueImportFromForm():
+	form = cgi.FieldStorage() # Create instance of FieldStorage 
 	emailID  = form.getvalue('emailIDs')
 	username  = form.getvalue('username')
 	time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	formGenTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S::%f')
+	formID = hasher(formGenTime)
 
+
+#salterForTokenGeneration
+def salter():
+	saltedEmailID = hasher(emailID)
+	saltedUsername = hasher(email)
+
+def tokenGenerator():
+    rawtoken = str(saltedEmailID)+str(saltedUsername)
+    tokenInit = salter(rawtoken)+str(formID)
+    token = str(tokenInit)+str(formID)
+
+
+
+
+def responsePreview():
+	
 	print "Content-type:text/html\r\n\r\n"
 	print "<html>"
 	print "<head>"
 	print "<title>Hello - Second CGI Program</title>"
 	print "</head>"
 	print "<body>"
-	print "<h6>token triggered: %s </h6>" % (tokenID)
+	print "<h6>token triggered: %s </h6>" % (token)
 	print "<h6>token form bind ID: %s </h6>" % (formID)
 	print "<h6>mailOriginForToken: %s </h6>" % (emailID)
 	print "<h6>Username: %s </h6>" % (username)
@@ -35,4 +49,12 @@ def responseHandler():
 
 
 #Main Function initiator
-responseHandler()
+def moduleCaller():
+	valueImportFromForm()
+	salter()
+	tokenGenerator()
+	responsePreview() #TO BE REMOVED
+	#responseHandler()
+
+
+moduleCaller()
