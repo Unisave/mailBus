7754f5fd2652083cc630d1d1fd1da569f417c1f4
Sooraj Antony <soorajantony@live.in>
2017-08-12 18:35:32 +0530

    Utils repo for mysql controller

--- dbUtils.py
+++ dbUtils.py
@@ -9,17 +9,17 @@ Created on Sat Aug 12 10:58:26 2017
 
 import MySQLdb
 
-def dbQueryExecutor():
+def dbQueryExecutor(execQuery):
     try:
         db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="umn1234", db="admin_logger")
-        cursor = db.cursor()        
-        cursor.execute("SHOW TABLES;")
+        cursor = db.cursor()
+        cursor.execute(execQuery)
         results = cursor.fetchall()
         # Check if anything at all is returned
         if results:
-            print results[0]
+            print results
         else:
-            print "getr"               
+            print "NO RESULTS RETURNED"               
     except MySQLdb.Error:
         print "ERROR IN CONNECTION"
     print "Execution completed"
