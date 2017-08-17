d50c8008391932340029cbba2e6afbb59e1cf35b
Sooraj Antony <soorajantony@live.in>
2017-08-17 17:12:37 +0530

    url and IP extractor development

--- returnImage.php
+++ returnImage.php
@@ -1,22 +1,40 @@
 <?php
-
-##PHP find public IP 
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
+$ip = file_get_contents('http://122.165.116.168/pyurl-dbupdater/ipExtractor.php');
 $path = "1.png";
-$python = `python hello.py .$ip.`;
-echo $python;
-    // if(file_exists($path))
-    // { 
-    //     header('Content-Length: '.filesize($path));
-    //     header('Content-Type: image/jpg');
-    //     header('Content-Disposition: inline; filename="'.$path.'";');
-    //     echo file_get_contents($path);
-    //     exit;
-    // }
+
+function curPageURL() {
+ $pageURL = 'http';
+ if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+ $pageURL .= "://";
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ }
+ return $pageURL;
+}
+
+$arr = (parse_url(curPageURL()));
+$output = array_slice($arr,1);
+echo implode("",$output);
+echo PHP_EOL;
+
+
+
+
+echo $ip
+// echo $_GET['id']
+// echo $_GET["id"]
+// $header = $ip.$token;
+// echo $header
+// $python = `python hello.py `;
+// echo $python;
+//     if(file_exists($path))
+//     { 
+//         header('Content-Length: '.filesize($path));
+//         header('Content-Type: image/jpg');
+//         header('Content-Disposition: inline; filename="'.$path.'";');
+//         echo file_get_contents($path);
+//         exit;
+//     }
 ?>
