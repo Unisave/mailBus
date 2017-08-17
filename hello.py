6173da57f1015aac37dbb5e9435bf45a2d99a292
Sooraj Antony <soorajantony@live.in>
2017-08-17 14:07:03 +0530

    Email opener IP tracker

--- hello.py
+++ hello.py
@@ -6,7 +6,13 @@ Created on Sat Aug 12 10:58:26 2017
 
 @author: Sooraj Antony
 """
+
+
+import sys
+
+ip = sys.argv[1]
 #write to file
 f = open('tempBuffer.file', 'a')
-f.write('hi there\n')  
+f.write('\n The ip is')  
+f.write(ip)  
 f.close()
--- returnImage.php
+++ returnImage.php
@@ -1,6 +1,11 @@
 <?php
+
+##PHP find public IP 
+$externalContent = file_get_contents('http://checkip.dyndns.com/');
+preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
+$ip = $m[1];
 $path = "1.png";
-$python = `python hello.py`;
+$python = `python hello.py .$ip.`;
 echo $python;
     if(file_exists($path))
     { 
