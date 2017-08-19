26bcab2fbf4748c9b9cfc1b3ea7aac88b4c8d21b
Sooraj Antony <soorajantony@live.in>
2017-08-19 11:46:20 +0530

    creating old token data updation service

--- dbUtils.py
+++ dbUtils.py
@@ -13,6 +13,42 @@ import MySQLdb
 from valHandler import location
 
 
+#variableDeclarations
+mysqlHostIP="192.168.1.14"
+mysqlUser="mailBot"
+mysqlPasswd="umn1234"
+mysqlDBName="mailBus"
+connectionErrorMsgMysql = "ERROR IN CONNECTION"
+executionCompleteMsg = "Execution completed"
+emptyResultsMsg = "NO RESULTS RETURNED"
+
+
+
+def valueUpdateAppender(varA, varB):
+    try:
+        dbA = MySQLdb.connect(host=mysqlHostIP, user=mysqlUser, passwd=mysqlPasswd, db=mysqlDBName) #AUTHENTICATION TO BE ENCRYPTED AND MOVED OUT SOON 
+
+        #enable MySQL tunnel proxy for security ssh user@host.com -L 9990:localhost:3306
+        cursorA = dbA.cursor()
+        
+        cursorA.execute (""" 
+            SELECT %s 
+            FROM tokenStatusHandler 
+            WHERE token = '%s'
+            """, 
+            (varA , varB)
+            )
+        outValue = cursorA.fetchall()
+        print varA + " is " + outValue
+        dbA.close
+        return(outValue)
+    except MySQLdb.Error:
+        print connectionErrorMsgMysql 
+        print MySQLdb.Error
+    print executionCompleteMsg
+
+
+
 
 
 
@@ -23,7 +59,7 @@ def noTokenExist(tokenNumber,tokenIP,triggerTime,geoJS):
         var3 = tokenIP
         var4 = " ("+unicode(geoJS['longitude']) +" :: "+ unicode(geoJS['latitude']) +") , "+ unicode(geoJS['city']) +" , "+ unicode(geoJS['region_name']) +" , "+ unicode(geoJS['zip_code']) +" , "+ unicode(geoJS['country_name'])
         print var4
-        db1 = MySQLdb.connect(host="192.168.1.14", user="mailBot", passwd="umn1234", db="mailBus") #AUTHENTICATION TO BE ENCRYPTED AND MOVED OUT SOON 
+        db1 = MySQLdb.connect(host=mysqlHostIP, user=mysqlUser, passwd=mysqlPasswd, db=mysqlDBName) #AUTHENTICATION TO BE ENCRYPTED AND MOVED OUT SOON 
         #enable MySQL tunnel proxy for security ssh user@host.com -L 9990:localhost:3306
         cursor1 = db1.cursor()
         cursor1.execute (""" INSERT INTO bad_Tokens_temp(token, TIMESTAMPER, badIP, geoTracker) 
@@ -36,21 +72,21 @@ def noTokenExist(tokenNumber,tokenIP,triggerTime,geoJS):
         responseNoToken = "BAD TOKEN ENTRY DETECTED FOR TOKEN NUMBER:: " + tokenNumber + " TRIGGERED AT " + triggerTime + " FROM " + tokenIP 
         print  responseNoToken
     except MySQLdb.Error:
-        print "ERROR IN CONNECTION"
+        print connectionErrorMsgMysql 
         print MySQLdb.Error
-    print "Execution completed"
+    print executionCompleteMsg
  
 
 
 
 
-def tokenExist(tokenNumber,tokenIP,triggerTime):
+def tokenExist(tokenNumber,tokenIP,triggerTime,geoJS):
     try:
         var1 = tokenNumber
         var2 = triggerTime
         var3 = tokenIP
-
-        db2 = MySQLdb.connect(host="192.168.1.14", user="mailBot", passwd="umn1234", db="mailBus") #AUTHENTICATION TO BE ENCRYPTED AND MOVED OUT SOON 
+        var4 = " ("+unicode(geoJS['longitude']) +" :: "+ unicode(geoJS['latitude']) +") , "+ unicode(geoJS['city']) +" , "+ unicode(geoJS['region_name']) +" , "+ unicode(geoJS['zip_code']) +" , "+ unicode(geoJS['country_name'])
+        db2 = MySQLdb.connect(host=mysqlHostIP, user=mysqlUser, passwd=mysqlPasswd, db=mysqlDBName) #AUTHENTICATION TO BE ENCRYPTED AND MOVED OUT SOON 
         #enable MySQL tunnel proxy for security ssh user@host.com -L 9990:localhost:3306
         cursor2 = db2.cursor()
         cursor2.execute ("""
@@ -65,23 +101,30 @@ def tokenExist(tokenNumber,tokenIP,triggerTime):
         if results2:
             print "Correct token (" + tokenNumber + ") TRIGGERED AT " + triggerTime + " FROM " + tokenIP 
             #SCRIPT TO LOG location and other details
+            TIMESTAMPER = valueUpdateAppender('TIMESTAMPER' , var1)
+            numberOfOpens = valueUpdateAppender('numberOfOpens' , var1)
+            openedLocationIP = valueUpdateAppender('openedLocationIP' , var1)
+            openGeoLocation = valueUpdateAppender('openGeoLocation' , var1)
+            print "Final added aggregated values::" + unicode(TIMESTAMPER)
+            print "Final added aggregated values::" + unicode(numberOfOpens)
+            print "Final added aggregated values::" + unicode(openedLocationIP)
+            print "Final added aggregated values::" + unicode(openGeoLocation)
         else:
-            print "NO RESULTS RETURNED"
+            print "NOT STARTED due to no copy"
             # cursor3.execute (" INSERT INTO mailBus.tokenStatusHandler (TIMESTAMPER, token, numberOfOpens, openedLocationIP, openGeoLocation) VALUES ('%s', '%s', '%s', '%s', '%s');" % (var2, var1, , var3, ))
-            
-
-        
-        results3 = cursor1.fetchall()
+        results3 = cursor3.fetchall()
         db2.commit()
         db2.close()
         print resultsa
         responseNoToken = "BAD TOKEN ENTRY DETECTED FOR TOKEN NUMBER:: " + tokenNumber + " TRIGGERED AT " + triggerTime + " FROM " + tokenIP 
         print  responseNoToken
     except MySQLdb.Error:
-        print "ERROR IN CONNECTION"
+        print connectionErrorMsgMysql 
         print MySQLdb.Error
-    print "Execution completed"
+    print executionCompleteMsg
  
+
+
 
 
 
@@ -92,7 +135,7 @@ def dbTokenStatusChecker(tokenNumber,tokenIP,triggerTime):
         # geoAddress = geoJS['longitude'] + "," + geoJS['latitude' + "," + geoJS['city'] + "," +  geoJS['region_name'] + "," +  geoJS['zip_code'] + "," +  geoJS['country_name']
         # print geoJS
         
-        db = MySQLdb.connect(host="192.168.1.14", user="mailBot", passwd="umn1234", db="mailBus") #AUTHENTICATION TO BE ENCRYPTED AND MOVED OUT SOON 
+        db = MySQLdb.connect(host=mysqlHostIP, user=mysqlUser, passwd=mysqlPasswd, db=mysqlDBName) #AUTHENTICATION TO BE ENCRYPTED AND MOVED OUT SOON 
         #enable MySQL tunnel proxy for security ssh user@host.com -L 9990:localhost:3306
         cursor = db.cursor()
         cursor.execute ("""
@@ -108,11 +151,11 @@ def dbTokenStatusChecker(tokenNumber,tokenIP,triggerTime):
         if results:
             print "Correct token (" + tokenNumber + ") TRIGGERED AT " + triggerTime + " FROM " + tokenIP 
             #SCRIPT TO LOG location and other details
+            tokenExist(tokenNumber,tokenIP,triggerTime,geoJS)
         else:
-            print "NO RESULTS RETURNED"
+            print emptyResultsMsg
             noTokenExist(tokenNumber,tokenIP,triggerTime,geoJS)
     except MySQLdb.Error:
-        print "ERROR IN CONNECTION"
-    print "Execution completed"
-
-
+        print connectionErrorMsgMysql 
+        print MySQLdb.Error
+    print executionCompleteMsg
