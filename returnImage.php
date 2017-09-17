a7965143196a1af2442ab4fced5c30097881d824
Sooraj Antony <soorajantony@live.in>
2017-09-17 15:19:18 +0530

    Main controller and ignored updation

--- returnImage.php
+++ returnImage.php
@@ -1,28 +1,13 @@
 <?php
 
 
-
-$path = "1.png";
-$token = $_GET['id'];
+//ACTUAL DATA GATHERER=========================================
+$path = "staticAssets/images/logo.png";
 $t=time();
+
+$token = $_GET['id'];
+
 $currTime = date("Y-m-d::H:i:s",$t);
-// $currTime = "bleh";
-
-
-
-// function curPageURL() {
-//     $pageURL = 'http';
-//     if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-//     $pageURL .= "://";
-//     if ($_SERVER["SERVER_PORT"] != "80") {
-//     $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-//     } else {
-//     $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-//     }
-//     return $pageURL;
-// }
-
-
 
 function currIP() {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) { 
@@ -34,67 +19,51 @@ function currIP() {
     }
     return $ip;
 }
-
-// $key = date("YmdHis",$t);
-// echo " time NOW ";
-// echo $currTime;
-// echo " time salt ";
-// echo $key;
-// $myVarIWantToEncodeAndDecode = currIP();
-// $key = "hacker";
-// $encoded = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $myVarIWantToEncodeAndDecode, MCRYPT_MODE_CBC, md5(md5($key))));
-// $decoded = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($encoded), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
-
-// echo "crypted";
-// echo $encoded;
-// echo "string";
-// echo $decoded;
-
-
 $ip = currIP(); 
+//=============================================================
+
+//MOCK SIMULATOR
+//====FALSE TOKEN======
+// $token = "asdasda";
+// $currTime = "5:60";
+// $ip = "5.5.5.5"; 
+//====================
+//====FALSE IP========
+// $token = "asdas";
+// $currTime = "5:60";
 // $ip = "hfd"; 
+//====================
+//====FALSE TIME======
+// $token = "asdas";
+// $currTime = "bleh";
+// $ip = "5.5.5.5"; 
+//====================
+
+
 
 if(1 === preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $ip))
 {
     if(1 === preg_match('/[^\d\:\-]/', $currTime))
     {
         $locked = $ip." ".$currTime;
-        // echo $locked;
-        // echo "Hacker alert1";
-        $pythonX = `python suspiciousEntry.py $locked`;
-        // echo $pythonX;
+        $pythonX1 = `python BackEnds/entryFilters/suspiciousEntry.py $locked`;
     } else
     {
         if(1 === preg_match('/^[^\W]+$/', $token)){
-            // $arr = (parse_url(curPageURL()));
-            // echo $ip;
-            // echo $token;
-            // echo "     asdasd     ";
             $header = $ip." ".$token." ".$currTime;
-            // echo $header;
-            $python = `python hello.py $header`;
-            // echo $python;
+            $python = `python BackEnds/entryFilters/recieverEntry.py $header`;
         } else
         {
             $locked = $ip." ".$currTime;
-            // echo $locked;
-            // echo "Hacker alert2";
-            $pythonX = `python suspiciousEntry.py $locked`;
-            // echo $pythonX;
+            $pythonX2 = `python BackEnds/entryFilters/suspiciousEntry.py $locked`;
         };
     };
 } else
 {
     $locked = $ip." ".$currTime;
-    // echo $locked;
-    // echo "Hacker alert3";
-    $pythonX = `python suspiciousEntry.py $locked`;
-    // echo $pythonX;
+    $pythonX3 = `python BackEnds/entryFilters/suspiciousEntry.py $locked`;
 };
 
-
-
-$path = "1.png"; 
     if(file_exists($path))
     { 
         header('Content-Length: '.filesize($path));
@@ -103,4 +72,6 @@ $path = "1.png";
         echo file_get_contents($path);
         exit;
     }
+
+
 ?>
